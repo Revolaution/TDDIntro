db3dd8587776f491449ed971cb12cb59a81f7b0d
Ryan Lau <ryanlau92@gmail.com>
2016-02-21 15:23:39 -0600

    Changed to add factorial style for 4 tests

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -2,9 +2,11 @@ package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        if (i > 1){
-            return i;
+        if (i == 0){
+            return 1;
         }
-        return 1;
+        else{
+            return i * compute(i - 1);
+        }
     }
 }

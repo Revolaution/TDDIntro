765ba593cb2d209e455a50d9ab7970fe77d1e05f
Ryan Lau <ryanlau92@gmail.com>
2016-02-21 15:33:37 -0600

    Finish factorial TDD

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -1,8 +1,11 @@
 package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
-    public Integer compute(int i) {
-        if (i == 0){
+    public Integer compute(int i) throws IllegalArgumentException {
+        if (i < 0){
+            throw new IllegalArgumentException("Factorial Numbers must be positive");
+        }
+        else if (i == 0){
             return 1;
         }
         else{
